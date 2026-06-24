f59c4e8a10f28e6201aadf3be722d42411c9ef18
Vitor Zago <vitor.zago8888@gmail.com>
2026-06-24 20:40:24 -0300

    Creating new directories.

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 import pygame
-import random
-import math
+from game.logic import Game
 
 pygame.init()
 
-# Tela
-WIDTH = 800
-HEIGHT = 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Defesa Planetária")
-
-# Cores
-WHITE = (255, 255, 255)
-BLUE = (0, 100, 255)
-RED = (255, 50, 50)
-BLACK = (0, 0, 0)
-YELLOW = (255, 255, 0)
-
-clock = pygame.time.Clock()
-
-# Planeta
-planet_x = WIDTH // 2
-planet_y = HEIGHT // 2
-planet_radius = 40
-planet_life = 5
-
-# Listas
-enemies = []
-bullets = []
-
-score = 0
-running = True
+game = Game()
+game.run()
